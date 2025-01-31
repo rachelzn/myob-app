7bd312a338e642c896780a8efaf0c41b78d7d4f2
“rachelzn” <racheltjengerari@gmail.com>
2025-01-31 12:49:29 +0700

    style: create style constants

---
+++ lib/styles/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Colorz {
+  static const ivoryWhite = Color(0xFFFAF3E0);
+  static const lemonYellow = Color(0xFFF4D03F);
+  static const goldenBrown = Color(0xFFB7950B);
+  static const charcoalNavy = Color(0xFF2C3E50);
+  static const royalBlue = Color(0xFF295F9B);
+  static const powderBlue = Color(0xFF7FB3D5);
+  static const lavenderMist = Color(0xFFD8BFD8);
+  static const tealGreen = Color(0xFF008080);
+  static const coralRed = Color(0xFFFF6F61);
+}
